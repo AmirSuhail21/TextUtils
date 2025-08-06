b00116ea4e3cec3d4961913d53cd4a58bba1de33
Amir Suhail <mannuraien3@gmail.com>
2025-08-06 13:03:04 +0530

    add paragraph

--- src/App.js
+++ src/App.js
@@ -2,12 +2,27 @@ import "./App.css";
 // import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
+import React, {useState} from 'react'
 
 function App() {
+  const [mode, setmode] = useState('light') // wheather dark mode is enable or not
+  const toggleMode = () =>{
+    if(mode === 'light'){ 
+      setmode('dark')
+      document.body.style.backgroundColor = 'rgb(18 18 65)';
+      document.body.style.color = 'white';
+
+    }else{
+      setmode ('light')
+      document.body.style.backgroundColor = 'white';
+            document.body.style.color = 'black';
+
+    }
+  }
   return (
     <>
 
-  <Navbar title = "Textutils" about = "About"/>
+  <Navbar title = "Textutils" mode = {mode} toggleMode = {toggleMode}/>
 
 
 <div className="container my-3">
